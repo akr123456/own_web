551906fe1c59af3b8d0e841585785b46a37ce2c6
AIGC-全栈工程师 <3306145091@qq.com>
2025-11-11 13:24:37 +0800

    contact-bg_delete & 透明度

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -28,7 +28,7 @@ export default function BlogLayout({
 
       <div className="fixed inset-0 flex justify-center sm:px-8">
         <div className="flex w-full max-w-7xl lg:px-8">
-          <div className="w-full bg-zinc-50/90 ring-1 ring-zinc-100 dark:bg-zinc-900/95 dark:ring-zinc-400/20" />
+          <div className="w-full bg-zinc-50/85 ring-1 ring-zinc-100 dark:bg-zinc-900/85 dark:ring-zinc-400/20" />
         </div>
       </div>
 
