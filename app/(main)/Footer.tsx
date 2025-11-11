917e9ad1a7250d6cd7fc20624d08187e38271bb1
AIGC-全栈工程师 <3306145091@qq.com>
2025-11-11 14:40:33 +0800

    底部任务栏去除

--- app/(main)/Footer.tsx
+++ app/(main)/Footer.tsx
@@ -1,12 +1,10 @@
 import { count, isNotNull } from 'drizzle-orm'
-import Link from 'next/link'
 import React from 'react'
 
 import { CursorClickIcon, UsersIcon } from '~/assets'
 import { PeekabooLink } from '~/components/links/PeekabooLink'
 import { Container } from '~/components/ui/Container'
 import { kvKeys } from '~/config/kv'
-import { navigationItems } from '~/config/nav'
 import { db } from '~/db'
 import { subscribers } from '~/db/schema'
 import { env } from '~/env.mjs'
@@ -14,35 +12,6 @@ import { prettifyNumber } from '~/lib/math'
 import { redis } from '~/lib/redis'
 
 import { Newsletter } from './Newsletter'
-
-function NavLink({
-  href,
-  children,
-}: {
-  href: string
-  children: React.ReactNode
-}) {
-  return (
-    <Link
-      href={href}
-      className="transition hover:text-lime-500 dark:hover:text-lime-400"
-    >
-      {children}
-    </Link>
-  )
-}
-
-function Links() {
-  return (
-    <nav className="flex gap-6 text-sm font-medium text-zinc-800 dark:text-zinc-200">
-      {navigationItems.map(({ href, text }) => (
-        <NavLink key={href} href={href}>
-          {text}
-        </NavLink>
-      ))}
-    </nav>
-  )
-}
 
 async function TotalPageViews() {
   let views: number
@@ -114,14 +83,13 @@ export async function Footer() {
             <div className="mx-auto mb-8 max-w-md">
               <Newsletter subCount={`${subs?.subCount ?? '0'}`} />
             </div>
-            <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
+            <div className="flex flex-col items-center gap-6 text-center sm:flex-row sm:text-left">
               <p className="text-sm text-zinc-500/80 dark:text-zinc-400/80">
                 &copy; {new Date().getFullYear()} Cali Castle. 网站已开源：
                 <PeekabooLink href="https://github.com/CaliCastle/cali.so">
                   GitHub
                 </PeekabooLink>
               </p>
-              <Links />
             </div>
           </Container.Inner>
           <Container.Inner className="mt-6">
