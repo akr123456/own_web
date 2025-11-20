1311a5930d85fbe3f2374f7f002d12b2a2dfd031
AIGC-全栈工程师 <3306145091@qq.com>
2025-11-20 15:25:32 +0800

    继续修复4

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -3,7 +3,7 @@ import './blog/[slug]/blog.css'
 import { Analytics } from '@vercel/analytics/react'
 import { Suspense } from 'react'
 
-import { Footer } from '~/app/(main)/Footer'
+import Footer from '~/app/(main)/Footer'
 import { Header } from '~/app/(main)/Header'
 import { QueryProvider } from '~/app/QueryProvider'
 
